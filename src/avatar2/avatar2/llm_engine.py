f608b0faf3e2d7dfe40a85ecffeb13cabf15ce06
Walleed Khan <walleedk@yorku.ca>
2024-05-21 17:53:43 -0400

    Caching separeted from llm engine

--- src/avatar2/avatar2/llm_engine.py
+++ src/avatar2/avatar2/llm_engine.py
@@ -10,7 +10,6 @@ from .llm_langchain import LLMLangChain
 from .llm_withfaces import LLMWithFaces
 from .llm_local_cache import LocalCache
 
-
 class LLMEngine(Node):
     def __init__(self):
         super().__init__('llm_engine_node')
@@ -42,7 +41,7 @@ class LLMEngine(Node):
             format = self.get_parameter('format').get_parameter_value().string_value
 
             self._llm = LLMLangChain(model=model, prompt=prompt, vectorstore=vectorstore, format=format)
-            self._local_cache = LocalCache()
+            self._local_cache = LocalCache(node=self)
         elif avatar_type == 'faces':
             self.declare_parameter('root', './museum/')
             root = self.get_parameter('root').get_parameter_value().string_value
@@ -60,52 +59,30 @@ class LLMEngine(Node):
             format = self.get_parameter('format').get_parameter_value().string_value
 
             self._llm = LLMWithFaces(model=model, prompt=prompt, vectorstore=vectorstore, format=format, node=self)
-            self._local_cache = LocalCache()
+            self._local_cache = LocalCache(node=self)
         else:
             self.get_logger().info(f'{self.get_name()} {avatar_type} not known, using dummy')
             self._llm = LLMDummy()
 
         self.create_subscription(TaggedString, inTopic, self._callback, QoSProfile(depth=1))
         self._publisher = self.create_publisher(TaggedString, outTopic, QoSProfile(depth=1))
-
-    def _get_response(self, query):
-        prompt, response = self._llm.response(text=query)
-        
-        return response
-    
-    def _evict_cache(self):
-        self.get_logger().info(f"{self.get_name()} The current cache contains {self._local_cache._cache.keys()}")
-        oldest = ((key for key in self._local_cache._cache if key not in self._local_cache._permanent_entries)).__next__()
-        self.get_logger().info(f"{self.get_name()} Evicting {oldest}")
-        if oldest:
-            self._local_cache._cache.pop(oldest)
-    
-    def _process_query(self, query):
-        query_normalized = query.lower().strip().translate(str.maketrans('', '', string.punctuation))
-        self.get_logger().info(f"{self.get_name()} Normalized query: {query_normalized}")
-        
-        if query_normalized in self._local_cache._cache:
-            self.get_logger().info(f"{self.get_name()} Cache hit for {query_normalized}")
-            self._local_cache._cache.move_to_end(query_normalized)
-            return self._local_cache._cache[query_normalized]
-        
-        self.get_logger().info(f"{self.get_name()} Cache miss for {query_normalized}")
-        response = self._get_response(query)
-        if len(self._local_cache._cache) >= self._local_cache._max_size:
-            self.get_logger().info(f"{self.get_name()} Cache full, purging oldest entry")
-            self._evict_cache()
-        self._local_cache._cache[query_normalized] = response
-        
-        return response
     
     def _callback(self, msg):
         """Deal with translation"""
         self.get_logger().info(f"{self.get_name()} listening got {msg.text.data}")
-
+        self.get_logger().info(f"{self.get_name()} checking cache for {msg.text.data}")
+        response = self._local_cache._process_query(msg.text.data)
+        
+        if response is None:
+            self.get_logger().info(f"{self.get_name()} cache miss for {msg.text.data}")
+            prompt, response = self._llm.response(text=msg.text.data)
+            self.get_logger().info(f"{self.get_name()} response: {response}")
+        self.get_logger().info(f"{self.get_name()} updating cache for {msg.text.data}")
+        self._local_cache._update_cache(msg.text.data, response)
+        
         tagged_string = TaggedString()
         tagged_string.header.stamp = self.get_clock().now().to_msg()
         tagged_string.audio_sequence_number = msg.audio_sequence_number
-        response = self._process_query(msg.text.data)
         tagged_string.text.data = str(response)
         self._publisher.publish(tagged_string)
 
--- src/avatar2/avatar2/llm_local_cache.py
+++ src/avatar2/avatar2/llm_local_cache.py
@@ -1,7 +1,13 @@
+import os
+import json
+import string
 from collections import OrderedDict
 
 class LocalCache():
-    def __init__(self):
+    def __init__(self, node, filename=None):
+        self._max_size = 100
+        self._node = node
+        self._node.get_logger().info(f"{self._node.get_name()} LocalCache alive")
         self._permanent_entries = set([
             'hi', 'hello', 'good bye', 'goodbye',
             'how are you', 'what is your name', 
@@ -18,4 +24,44 @@ class LocalCache():
             'good evening': {"Good evening! How can I assist you today?"},
             'good night': {"Good evening! How can I assist you today?"},
         })
-        self._max_size = 100
+        
+        if filename:
+            self._load_cache(filename)
+            
+    def _load_cache_fom_file(self, filename):
+        if os.path.exists(filename):
+            try:
+                with open(filename, 'r') as f:
+                    data = json.load(f)
+                    for key, value in data.items():
+                        self._cache[key] = value
+                    self.getlogger().info(f"{self._node.get_name()} Loaded cache entries from {filename} with {len(data)} entries")
+            except Exception as e:
+                self._node.error(f"{self._node.get_name()} Error loading cache from {filename}: {e}")
+        else:
+            self._node.get_logger().info(f"{self._node.get_name()} No cache file found at {filename}")
+    
+    def _evict_cache(self):
+        self._node.get_logger().info(f"{self._node.get_name()} The current cache contains {self._cache.keys()}")
+        oldest = ((key for key in self._cache if key not in self._permanent_entries)).__next__()
+        self._node.get_logger().info(f"{self._node.get_name()} Evicting {oldest}")
+        if oldest:
+            self._cache.pop(oldest)
+    
+    def _process_query(self, query):
+        query_normalized = query.lower().strip().translate(str.maketrans('', '', string.punctuation))
+        self._node.get_logger().info(f"{self._node.get_name()} Normalized query: {query_normalized}")
+        
+        if query_normalized in self._cache:
+            self._node.get_logger().info(f"{self._node.get_name()} Cache hit for {query_normalized}")
+            self._cache.move_to_end(query_normalized)
+            return self._cache[query_normalized]
+        
+        self._node.get_logger().info(f"{self._node.get_name()} Cache miss for {query_normalized}")
+        response = None
+        
+    def _update_cache(self, query_normalized, response) -> None:
+        if len(self._cache) >= self._max_size:
+            self._node.get_logger().info(f"{self._node.get_name()} Cache full, purging oldest entry")
+            self._evict_cache()
+        self._cache[query_normalized] = response
